523b379ff765c2791472d061b5d3921df3aa9050
Guillermo Gardella <guillegardella@gmail.com>
2023-11-06 21:18:01 -0300

    Problemas con intelliJ

--- Back/alquicancha/src/main/java/com/alquicancha/entity/User.java
+++ Back/alquicancha/src/main/java/com/alquicancha/entity/User.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable=false)
